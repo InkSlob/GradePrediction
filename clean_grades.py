b72af2828473d56962fcabb868e54ad038721130
InkSlob <jessewrigh@yahoo.com>
2016-05-31 12:06:20 -0400

    final csv for grades to be loaded into ML

--- clean_grades.py
+++ clean_grades.py
@@ -14,20 +14,24 @@ f.close()
 
 # grades 2 dimension list, inner list is 4 elements
 #  a new dictionary for grades, will be a dictionary keyed by student id.  
-#  each value in this dictionary will hold another dictionary for the specific grades.
 d_grades = dict()
-grade_list = []
 current_student_grades = []
 
 length = len(grades)    # 11,101 rows in grades
 
+for row in range(1, length):
+    student_id = int(grades[row][0])
+    d_grades[student_id] = ""
+
+
 for row in range(1, length):        # skip first row, header.
         # extract needed data
-        student_id = str(grades[row][0])
+        student_id = int(grades[row][0])
         grade_calculated = float(grades[row][2]) / float(grades[row][3])
-        assignment_name = grades[row][1]
+        assignment_name = str(grades[row][1])
         
         # Make a list to hold assignment and calculated grade [assignment name, grade]
+        grade_list = []
         grade_list.append(assignment_name) 
         grade_list.append(grade_calculated)
         
@@ -35,42 +39,47 @@ for row in range(1, length):        # skip first row, header.
         # student id already in the dictionary
         if student_id in d_grades:
             # extract grades from dictionary value by key
-            current_student_grades = d_grades[student_id]
+            #current_student_grades = d_grades.get(student_id)
+            #print "ID: ", student_id, " : ", current_student_grades
             # add new assignment and grade to the student id in dict
-            current_student_grades = current_student_grades + grade_list
-            d_grades[student_id] = current_student_grades
+            #current_student_grades = current_student_grades + grade_list
+            d_grades[student_id] += str(grade_list[0]) 
+            d_grades[student_id] += ", "
+            d_grades[student_id] += str(grade_list[1]) 
+            d_grades[student_id] += ", "
         else:
-            d_grades[student_id] = grade_list
+            print "ERROR IN Dict!"
+            
         # clear grade list              
         grade_list[:] = []
-        
-        
-# The if then statement for first entry is adding the first entry twice.
-dict_len_original = len(d_grades)
 
+# List holding grades extracted from dictionary
+grades_all = []
+# List holding individual grades
+id_list = []
 
-# Removing the entries that are not complete.  Not sure why there are ids with only a couple grades
-key_list = []
-
-# find dictionary entries that have less than 5 grades
+# loop through dict and split each id into an array, append these to grades_all[]
 for key in d_grades:
-    if len(d_grades[key]) < 10:
-        key_list.append(key)
+    dict_spot = str(d_grades.get(key))
+    #print dict_spot
+    id_list = dict_spot.split(',')
+    #print id_list
+    grades_all.append(id_list)
+    #id_list[:] = []
 
-# remove the entries in dict that have less than 5 grades
-for k in key_list:
-    del d_grades[k]
+for j in range(0, len(grades_all)):
+    if len(grades_all[j]) < 10:
+        grades_all[j] = ''
 
-print "start: ", dict_len_original, " end: ", len(d_grades)
+final_grades = []
 
-# lazy approach to remove the duplicate 1 & 2 elements
-for k in d_grades:
-    entry = d_grades[k]
-    # recall 2 elements equals an entry
-    first, second = entry[0], entry[2]
-    if first == second:
-        # remove first entry
-        entry = entry[2:]
-        d_grades[k] = entry
+for i in grades_all:
+    if (len(i)>0):
+        final_grades.append(i)
 
-print d_grades['744']
+with open("cs120_tabulated_grades.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(final_grades)
+
+
+
